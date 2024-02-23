ae6c5194f30fcc04541f7c627e1629d1a746607a
biohazard63 <quentindevaulx63@gmail.com>
2024-02-23 09:46:56 +0100

    combatant ramdom

--- assets/js/arena.js
+++ assets/js/arena.js
@@ -1,17 +1,33 @@
-let warriorAxe = new WarriorAxe("Axe Warrior", 10, 100);
-let warriorSword = new WarriorSword("Sword Warrior", 15, 100);
-let warriorSpear = new WarriorSpear("Spear Warrior", 5, 100);
+let warriors = [
+  new WarriorAxe("Axe Warrior", 10, 100),
+  new WarriorSword("Sword Warrior", 15, 100),
+  new WarriorSpear("Spear Warrior", 5, 100)
+];
 
-
-while (warriorSpear.isAlive() && warriorSword.isAlive()) {
-  warriorSpear.attack(warriorSword);
-  warriorSword.attack(warriorSpear);
+function getRandomWarrior() {
+  let index = Math.floor(Math.random() * warriors.length);
+  return warriors[index];
 }
 
-if (warriorSpear.isAlive() && warriorSword.isAlive()) {
+let warrior1 = getRandomWarrior();
+let warrior2 = getRandomWarrior();
+
+while (warrior1 === warrior2) {
+  warrior2 = getRandomWarrior();
+}
+
+console.log(warrior1.name);
+console.log(warrior2.name);
+
+while (warrior1.isAlive() && warrior2.isAlive()) {
+  warrior1.attack(warrior2);
+  warrior2.attack(warrior1);
+}
+
+if (warrior1.isAlive() && warrior2.isAlive()) {
   console.log("It's a draw");
-} else if (warriorSpear.isAlive()) {
-  console.log(`${warriorSpear.name} wins`);
+} else if (warrior1.isAlive()) {
+  console.log(`${warrior1.name} wins`);
 } else {
-  console.log(`${warriorSword.name} wins`);
+  console.log(`${warrior2.name} wins`);
 }
